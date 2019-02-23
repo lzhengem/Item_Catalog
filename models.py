8b05d0bd61ed27ef80f2b188190c623d82ef6d05
Lena <lzhengem@gmail.com>
2019-02-23 13:53:19 -0800

    chore: changed category_id in item to cat_id

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class Item(Base):
     id = Column(Integer, primary_key = True)
     title = Column(String, index = True, nullable=False, unique=True)
     description = Column(String)
-    category_id = Column(Integer, ForeignKey('category.id'))
+    cat_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
 
     @property
