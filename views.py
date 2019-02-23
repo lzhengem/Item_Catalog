54081d78d613e401305c7763b345fc854c2a41a7
Lena <lzhengem@gmail.com>
2019-02-23 09:57:10 -0800

    feat: added endpoints edit, delete, item for viewing

--- views.py
+++ views.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 def homepage():
     return "You are at the homepage!"
 
+@app.route("/catalog/<category>/items/")
+def catalog(category):
+    return "You are viewing %s items!" % category
+
+@app.route("/catalog/<category>/edit/")
+def edit(category):
+    return "You are viewing %s edit page!" % category
+
+
+@app.route("/catalog/<category>/delete/")
+def delete(category):
+    return "You are viewing %s delete page!" % category
+
+
+@app.route("/catalog/<category>/<item>/")
+def item(category,item):
+    return "You are viewing %s in %s!" % (item,category)
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0',port=5000)
