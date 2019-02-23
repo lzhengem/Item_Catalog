60f0db1616e2f989dd0b2a2150fb80b544bb7787
Lena <lzhengem@gmail.com>
2019-02-23 12:15:54 -0800

    feat: added models.py to create category and item tables in database

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key = True)
+    name = Column(String, index = True, nullable=False)
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key = True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+engine=create_engine('postgresql+psycopg2:///item_catalog')
+Base.metadata.create_all(engine)
