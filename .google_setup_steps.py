8306334785b6236804a8c8e34d3b2e9227e621b4
Lena <lzhengem@gmail.com>
2019-03-05 21:42:13 -0800

    doc: need to add both http and https for google login auth for heroku

--- .google_setup_steps.py
+++ .google_setup_steps.py
@@ -2,9 +2,9 @@ notes:
 -create an app with google developers
 -get client id and key
 -when making google log in, make sure to add 
-    Authorized JavaScript origins:      http://localhost:8000
+    Authorized JavaScript origins:      http://localhost:8000, http://lz-item-catalog.herokuapp.com, https://lz-item-catalog.herokuapp.com
     and
-    Authorized redirect URIs: http://localhost:8000/login http://localhost:8000/gconnect
+    Authorized redirect URIs: http://localhost:8000/login http://localhost:8000/gconnect, http://lz-item-catalog.herokuapp.com/login, http://lz-item-catalog.herokuapp.com/gconnect, https://lz-item-catalog.herokuapp.com/login, https://lz-item-catalog.herokuapp.com/gconnect
 -if getting the error:
     'app ____ is not an authorized endpoint blah blah blah' and you have already added that endpoint in google developer console,
     this might be because you have your app cached. go into private mode or clear your cache
