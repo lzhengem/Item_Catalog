bd13f07a1abb9f0a836d952aa89cce7f648f5bc2
Lena <lzhengem@gmail.com>
2019-03-09 21:05:08 +0000

    feat: added drop_tables, which drops the tables Item, User, and Category from the database

---
+++ drop_tables.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+from models import Base, Category, Item, User
+import os
+
+# if it is production, then use the database url
+# if it is not production, use psycopg2
+if os.getenv('FLASK_ENV') == 'production':
+    database_url = os.getenv('DATABASE_URL')
+    engine = create_engine(database_url)
+else:
+    engine = create_engine('postgresql+psycopg2:///item_catalog')
+
+Base.metadata.drop_all(bind=engine, tables=[Item.__table__, User.__table__, Category.__table__])
